44d95b692ce07961ba7b59469ec0f1f992b51fa2
Laowalter <fffregister@gmail.com>
2020-03-18 17:52:59 +0800

    sync finished for update2db.go

--- myimage/update/update2db.go
+++ myimage/update/update2db.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"crypto/md5"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"image/jpeg"
 	"io"
@@ -19,8 +20,7 @@ import (
 	"time"
 
 	"github.com/disintegration/imaging"
-	"github.com/photoServer"
-	"github.com/photoServer/myimage"
+	"github.com/photoServer/global"
 
 	. "github.com/logrusorgru/aurora"
 	"go.mongodb.org/mongo-driver/bson"
@@ -28,9 +28,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	database, collection, uri := "album", "pic", "mongodb://localhost:27017"
-	path := "/home/walter/go/src/github.com/photoServer/testData"
+func connectToDB(uri, dbname string) (*mongo.Database, error) {
 
 	clientOptions := options.Client().ApplyURI(uri)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
@@ -38,30 +36,48 @@ func main() {
 		fmt.Println("mongo.Connect() ERROR:", err)
 		os.Exit(1)
 	}
-	col := client.Database(database).Collection(collection)
+	return client.Database(dbname), nil
+}
 
-	mod := mongo.IndexModel{
-		Keys: bson.M{
-			"md5": 1, // index in ascending order, md5 is from the `bson: "md5"`
-		}, Options: options.Index().SetUnique(true),
-	}
-	ind, err := col.Indexes().CreateOne(context.TODO(), mod)
-	if err != nil { // Check if the CreateOne() method returned any errors
-		fmt.Println("Indexes().CreateOne() ERROR:", err)
-		os.Exit(1)
-	} else { // API call returns string of the index name
-		fmt.Println("CreateOne() index:", ind)
-	}
-	files := myimage.ListPics(path)
+func main() {
+	pathPtr := flag.String("path", ".", "Input a new path")
+	flag.Parse()
+
+	files := global.ListPics(*pathPtr) //仅处理图像找到截图视频exif信息再说。
 	total := len(files)
+
+	if total == 0 {
+		fmt.Printf("No files in directory: %v need to update!\n", Red(*pathPtr))
+		return
+	}
+
 	fmt.Printf("Total files number is: %v\n", total)
+	database, collection, uri := "album", "pic", "mongodb://localhost:27017"
+
+	db, _ := connectToDB(uri, database)
+	col := db.Collection(collection)
+
+	{ //Create index by md5
+		mod := mongo.IndexModel{
+			Keys: bson.M{
+				"md5": 1, // index in ascending order, md5 is from the `bson: "md5"`
+			}, Options: options.Index().SetUnique(true),
+		}
+		ind, err := col.Indexes().CreateOne(context.TODO(), mod)
+		if err != nil { // Check if the CreateOne() method returned any errors
+			fmt.Println("Indexes().CreateOne() ERROR:", err)
+			os.Exit(1)
+		} else { // API call returns string of the index name
+			fmt.Println("CreateOne() index:", ind)
+		}
+	}
 
 	i := 0
 	for _, file := range files {
 
 		i++
 		fmt.Printf("Dealing with %v/%v......", i, total)
-		document := &photoServer.Document{
+		document := &global.Document{
 			FileName:    filepath.Base(file),
 			Path:        file,
 			CreateTime:  time.Now(),
@@ -84,9 +100,10 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			if count == 1 {
-				fmt.Printf("File: %v already exist\n", Red(document.Path))
-
+			if count >= 1 {
+				update := bson.M{"$set": bson.M{"filename": document.FileName, "path": document.Path}}
+				_ = col.FindOneAndUpdate(context.TODO(), filter, update)
+				fmt.Printf("File: %v already exist, filename and path updated.\n", Red(document.Path))
 			} else {
 				fmt.Println(Red("Md5 is not unique index in database! Wrong"))
 				os.Exit(1)
@@ -106,14 +123,12 @@ func thumb(file string) []byte { //file: wholepath contains filename.
 	if err != nil {
 		panic(err)
 	}
-
 	thumb := imaging.Thumbnail(img, 200, 200, imaging.CatmullRom)
 	buf := new(bytes.Buffer)
 	err = jpeg.Encode(buf, thumb, nil)
 	if err != nil {
 		panic(err)
 	}
-
 	return buf.Bytes()
 }
 
@@ -131,5 +146,4 @@ func fileMd5(filePath string) (string, error) {
 	hashInBytes := hash.Sum(nil)[:16]
 	returnMD5String = hex.EncodeToString(hashInBytes)
 	return returnMD5String, nil
-
 }
