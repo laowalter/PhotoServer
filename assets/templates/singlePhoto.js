916c0c35910549b0e731f23f8419b1e50eb22c78
Laowalter <fffregister@gmail.com>
2020-05-23 19:15:05 +0800

    first step on enable fullscreen for single photo

--- assets/templates/singlePhoto.js
+++ assets/templates/singlePhoto.js
@@ -1,25 +1,4 @@
 $(function(){
-    $(".imgfullscreen").on("click", function(){
-        var $this = $(this);
-        if ($this.hasClass('clicked')){//dblclick for select
-            $this.removeClass('clicked'); 
-            //Double Click codes begin here
-            $("#overlay").css("display", function(){
-                return $("#overlay").css("display") == "none" ? "block":"none";
-            });
-
-        }else{ //single click for enlarge a single pic
-            $this.addClass('clicked');
-            setTimeout(function() { 
-                if ($this.hasClass('clicked')){
-                    $this.removeClass('clicked'); 
-                    //Single Click codes begin here
-                    parent.history.back();
-                }
-            }, 700);          
-        }
-    });
-
     $('body').keyup(function(e){
        if(e.keyCode == 8){
            // user has pressed backspace
@@ -30,4 +9,11 @@ $(function(){
          window.open("single?&index="+index, "_self");
        }
     });
+
+    // (function(){
+    //     alert("hi");
+    // })();
+
+
 })
+
